3f86dc648094ec9200ecbf4dfd91aba6d24b0684
Igcalinos-Christian <igcalchristian@gmail.com>
2026-02-03 20:22:05 +0800

    removed redundant code lines and converted functional js into OOP

--- js-map.js
+++ js-map.js
@@ -1,26 +1,52 @@
-var map = L.map('map').setView([8.359997, 124.868352], 17);
+var map = L.map('map').setView([8.359997, 124.868352], 17); //reference variable for the methods in "Map".
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+class Map{
 
-//fetch map coords and data from json file
-fetch('map.json')
-  .then(response => response.json())
-  .then(jsonData => {
+  init(){
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-    // Add polygon to the map
-    var polygon = L.polygon(jsonData[0].map_polygon_vertices, { color: 'green' })
+    this.Mpoly('map.json');
+    this.Mpins('map.json');
+  }
+
+  Mpoly(filename){
+
+    if(filename == null || filename == ""){
+      console.log("NO JSON FILE DETECTED!"); //to inform if json file was NOT receieved
+    }else{
+      console.log(filename) //to confirm if json file was recieved
+
+      //fetch map coords and data from json file
+      fetch(filename)
+      .then(response => response.json())
+      .then(jsonData => {
+        // Add polygon to the map
+        L.polygon(jsonData[0].map_polygon_vertices, { color: 'green' })
           .addTo(map)
-          .bindPopup(jsonData[0].map_name);
+          .LbindPopup(jsonData[0].map_name);
+        })
+      .catch(error => console.error('Error fetching JSON:', error));
+      }   
+  }
 
-    // Add pins to the map
-    jsonData[0].map_pins.forEach(function (pin) {
-      L.marker([pin.pin_lat, pin.pin_long]).addTo(map)
-        .bindPopup(pin.pin_name + '<br>Created by: ' + pin.pin_created_by);
-    });
-    map.fitBounds(marker.getBounds());
-})
+  Mpins(MapPins){
+    console.log(MapPins);
 
-.catch(error => console.error('Error fetching JSON:', error));
+    fetch(MapPins)
+      .then(response => response.json())
+      .then(jsonData => {
+        // Add pins to the map
+        jsonData[0].map_pins.forEach(function (pin) {
+          L.marker([pin.pin_lat, pin.pin_long]).addTo(map)
+            .bindPopup(pin.pin_name + '<br>Created by: ' + pin.pin_created_by);
+        });
+        map.fitBounds(marker.getBounds());
+        })
+  }
+}
+
+const MyMap = new Map();
+MyMap.init();
